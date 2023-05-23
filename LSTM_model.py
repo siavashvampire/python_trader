a8530155c3741d7a889121f6d5975bd3671fd726
siavash <sepahisiavash@gmail.com>
2023-05-23 21:59:24 +0330

    cleaning

--- LSTM_model.py
+++ LSTM_model.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 from training_tf import *
 
 
-def x_y_extract(df, n = 60):
+def x_y_extract(df, n=60):
     list_x = []
     list_y = []
 
@@ -15,7 +15,7 @@ def x_y_extract(df, n = 60):
         if ind + n + 1 > len(df):
             break
         if df.index[ind + n] - df.index[ind] == df.index[0 + n] - df.index[0]:
-            list_x.append(df['Close'][range(ind,ind+n)].values)
+            list_x.append(df['Close'][range(ind, ind + n)].values)
             list_y.append(df["signal1"][ind + n])
 
     x = np.array(list_x)
@@ -23,7 +23,7 @@ def x_y_extract(df, n = 60):
     return x, y
 
 
-def lstm_model(X_train, y_train, X_val, y_val, n = 60):
+def lstm_model(x_train, y_train, x_val, y_val, n=60):
     model = Sequential([layers.Input((n, 1)),
                         layers.LSTM(64),
                         layers.Dense(32, activation='relu'),
@@ -36,19 +36,18 @@ def lstm_model(X_train, y_train, X_val, y_val, n = 60):
     model.compile(loss=tf.keras.losses.binary_crossentropy,
                   optimizer=Adam(learning_rate=0.001),
                   metrics=[tf.keras.metrics.BinaryAccuracy(name='accuracy'),
-                      tf.keras.metrics.Precision(name='precision'),
-                      tf.keras.metrics.Recall(name='recall')])
+                           tf.keras.metrics.Precision(name='precision'),
+                           tf.keras.metrics.Recall(name='recall')])
 
-    history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=20, callbacks = [cback])
+    history = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=20, callbacks=[cback])
     return model, history
 
 
 def main():
     df = pd.read_csv('test_df.csv')
-    x,y = x_y_extract(df, n=60)
+    x, y = x_y_extract(df, n=60)
     y = y_encoder(y)
     x_train, y_train, x_val, y_val, x_test, y_test = test_train(x, y)
     model, history = lstm_model(x_train, y_train, x_val, y_val)
     model_plot(history)
     results(model, x_test, y_test)
-
