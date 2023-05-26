d8ab18ae4463019febd19c93cdc310536c9ff012
siavash <sepahisiavash@gmail.com>
2023-05-26 11:37:11 +0330

    cleaning

--- test2.py
+++ test2.py
@@ -3,6 +3,4 @@ from app.ml.model.main import learn
 csv_file_path_in = 'app/ml/file/EURUSD_M1_202301251915_202305031504.csv'
 csv_file_path_out = 'app/ml/file/EURUSD_indicator.csv'
 
-learn(csv_file_path_in,csv_file_path_out)
-
-
+learn(csv_file_path_in, csv_file_path_out)
