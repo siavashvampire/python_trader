39dd5af23e709993db8761d91411e80de791a84c
siavash <sepahisiavash@gmail.com>
2023-05-26 11:29:59 +0330

    fix bug

--- app/ml/model/indicator_extraction.py
+++ app/ml/model/indicator_extraction.py
@@ -189,16 +189,3 @@ def winning_policy_1(df_in, treshold):
     return df_in
 
 
-def main():
-    csv_file = 'app\ml\\file\EURUSD_M1_202301251915_202305031504.csv'
-    df = metatrader_data_import(csv_file)
-    df = adding_raw_indicators(df)
-    df = adding_indicator_signal(df)
-    df = solving_nans(df)
-    df = adding_percent_change(df)
-    df = winning_policy_1(df, 0.03)
-    print(df)
-    df.to_csv('app\ml\\file\EURUSD_indicator.csv', index=False, encoding='utf-8')
-
-
-main()
--- test2.py
+++ test2.py
@@ -1,7 +1,13 @@
+from app.ml.model.indicator_extraction import metatrader_data_import, adding_raw_indicators, adding_indicator_signal, \
+    solving_nans, adding_percent_change, winning_policy_1
 
+csv_file = 'app/ml/file/EURUSD_M1_202301251915_202305031504.csv'
+df = metatrader_data_import(csv_file)
+df = adding_raw_indicators(df)
+df = adding_indicator_signal(df)
+df = solving_nans(df)
+df = adding_percent_change(df)
+df = winning_policy_1(df, 0.03)
+print(df)
+df.to_csv('app\ml\\file\EURUSD_indicator.csv', index=False, encoding='utf-8')
 
-# msg = tpqoa_api.stream_one_data('EUR_USD')
-
-# print(msg.time, float(msg.bids[0].dict()['price']), float(msg.asks[0].dict()['price']))
-
-print()
