475873f18edb6ad0749c3cc12720f1cde77e391a
siavash <sepahisiavash@gmail.com>
2023-06-13 23:33:34 +0330

    add open_trade_window to data connector

--- app/data_connector/model/data_connector.py
+++ app/data_connector/model/data_connector.py
@@ -1,11 +1,19 @@
+from time import sleep
+
 import pandas as pd
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 from app.data_connector.model.enums import APIUsed
-from app.oanda.api import get_real_time_data_oanda, get_history_oanda, get_last_candle_oanda, create_order_oanda
+from app.oanda.api import get_real_time_data_oanda, get_history_oanda, get_last_candle_oanda, create_order_oanda, \
+    trade_window_url_oanda
 from core.config.Config import api_used
+from selenium import webdriver
 
 
 class DataConnector:
+    driver: WebDriver
     api_enums: APIUsed
 
     def __init__(self):
@@ -16,8 +24,26 @@ class DataConnector:
             self.get_history = get_history_oanda
             self.get_last_candle = get_last_candle_oanda
             self.create_order = create_order_oanda
+            self.trade_window_url = trade_window_url_oanda
 
     @staticmethod
     def get_history_from_file(name: str) -> pd.DataFrame:
         df = pd.read_csv(name)
         return df
+
+    def open_trade_window(self):
+        # option = Options()
+        # option.add_argument("--kiosk")
+        # option.add_argument("--start-fullscreen")
+        # self.driver = webdriver.Chrome(options=option)
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.trade_window_url)
+        sleep(40)
+        username_element = self.driver.find_element(By.ID, "username")
+        password_element = self.driver.find_element(By.ID, "password")
+
+        username_element.send_keys("sepahisiavash@gmail.com")
+        password_element.send_keys("VamPire1468")
+        sign_in_button = self.driver.find_element(By.XPATH, '/html/body/main/section/div/div/div/form/div[3]/button')
+        sign_in_button.click()
+
