5da8549ac9bc156de16e6c6bed62f2d1adfa1d4a
siavash <sepahisiavash@gmail.com>
2023-06-13 23:33:46 +0330

    complete PredictEnums

--- app/ml_avidmech/model/enums.py
+++ app/ml_avidmech/model/enums.py
@@ -1,5 +1,48 @@
-class PredictEnums:
+main_unit = 1000
+
+
+class PredictSellEnums:
     def __init__(self):
-        self.sell = 0
-        self.buy = 1
-        self.neutral = 2
+        self.id = 0
+
+    @staticmethod
+    def get_unit() -> int:
+        return -main_unit
+
+    def __repr__(self) -> str:
+        return "Sell"
+
+
+class PredictBuyEnums:
+    def __init__(self):
+        self.id = 1
+
+    @staticmethod
+    def get_unit() -> int:
+        return main_unit
+
+    def __repr__(self) -> str:
+        return "Buy"
+
+
+class PredictNeutralEnums:
+    def __init__(self):
+        self.id = 2
+
+    @staticmethod
+    def get_unit() -> int:
+        return 0
+
+    def __repr__(self) -> str:
+        return "Neutral"
+
+
+class PredictEnums:
+    neutral: PredictNeutralEnums
+    buy: PredictBuyEnums
+    sell: PredictSellEnums
+
+    def __init__(self):
+        self.sell = PredictSellEnums()
+        self.buy = PredictBuyEnums()
+        self.neutral = PredictNeutralEnums()
