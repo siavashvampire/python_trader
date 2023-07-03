334599a74d47fadcc0814556ce0955d2f99da366
siavash <sepahisiavash@gmail.com>
2023-07-03 18:36:51 +0330

    localy import in data connector

--- app/data_connector/model/data_connector.py
+++ app/data_connector/model/data_connector.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
 from app.data_connector.model.enums import APIUsed
-from app.oanda.api import get_real_time_data_oanda, get_history_oanda, get_last_candle_oanda, create_order_oanda, \
-    trade_window_url_oanda, prepare_api_oanda, close_api_oanda, get_balance_oanda, open_trade_window_oanda
-from app.quotex.api import trade_window_url_quotex, prepare_api_quotex, create_order_quotex, get_balance_quotex, \
-    close_api_quotex, get_real_time_data_quotex, open_trade_window_quotex, get_last_candle_quotex, get_history_quotex
+
 from core.config.Config import api_used
 
 
@@ -15,6 +12,9 @@ class DataConnector:
         self.api_enums = APIUsed()
 
         if api_used == self.api_enums.oanda:
+            from app.oanda.api import get_real_time_data_oanda, get_history_oanda, get_last_candle_oanda, \
+                create_order_oanda, trade_window_url_oanda, prepare_api_oanda, close_api_oanda, get_balance_oanda, \
+                open_trade_window_oanda
             self.trade_window_url = trade_window_url_oanda
             self.prepare_api = prepare_api_oanda
             self.get_real_time_data = get_real_time_data_oanda
@@ -26,15 +26,17 @@ class DataConnector:
             self.open_trade_window = open_trade_window_oanda
 
         if api_used == self.api_enums.quotex:
-            self.trade_window_url = trade_window_url_quotex
-            self.prepare_api = prepare_api_quotex
-            self.get_real_time_data = get_real_time_data_quotex
-            self.get_last_candle = get_last_candle_quotex
-            self.create_order = create_order_quotex
-            self.get_balance = get_balance_quotex
-            self.get_history = get_history_quotex
-            self.close_api = close_api_quotex
-            self.open_trade_window = open_trade_window_quotex
+            from app.quotex.api import trade_window_url_quotex_main, qx_api_class
+
+            self.trade_window_url = trade_window_url_quotex_main
+            self.prepare_api = qx_api_class.prepare_api_quotex
+            self.get_real_time_data = qx_api_class.get_real_time_data_quotex
+            self.get_last_candle = qx_api_class.get_last_candle_quotex
+            self.create_order = qx_api_class.create_order_quotex
+            self.get_balance = qx_api_class.get_balance_quotex
+            self.get_history = qx_api_class.get_history_quotex
+            self.close_api = qx_api_class.close_api_quotex
+            self.open_trade_window = qx_api_class.open_trade_window_quotex
 
     @staticmethod
     def get_history_from_file(name: str) -> pd.DataFrame:
