dde28fa9d1afc419303050578db5bd35ff623df1
siavash <sepahisiavash@gmail.com>
2023-07-15 16:57:11 +0330

    fix ui

--- core/theme/color/color.py
+++ core/theme/color/color.py
@@ -6,17 +6,17 @@ PB_BG_color_deactivate: str = "{r}, {g}, {b}, {a}".format(r=230, g=230, b=235, a
 PB_Text_color_deactivate: str = "{r}, {g}, {b}, {a}".format(r=200, g=200, b=200, a=255)  # push button Text color when
 # they deactivate
 
-PB_BG_color_active: str = "{r}, {g}, {b}, {a}".format(r=60, g=120, b=160, a=255)  # push a button background
+PB_BG_color_active: str = "{r}, {g}, {b}, {a}".format(r=52, g=63, b=90, a=255)  # push a button background
 # color when they activate
 PB_Text_color_active: str = "{r}, {g}, {b}, {a}".format(r=240, g=240, b=240, a=255)  # push button Text color when
 # they activate
 
 # line_edit_BG: str = "transparent"  # line edit background
-line_edit_BG: str = "{r}, {g}, {b}, {a}".format(r=60, g=120, b=160, a=255)  # line edit background color
+line_edit_BG: str = "{r}, {g}, {b}, {a}".format(r=52, g=63, b=90, a=255)  # line edit background color
 line_edit_Text_color: str = "{r}, {g}, {b}, {a}".format(r=240, g=240, b=240, a=255)  # line edit Text color
 
 label_Text_BG: str = "transparent"  # label background color
-label_Text_color: str = "{r}, {g}, {b}, {a}".format(r=0, g=90, b=90, a=255)  # label Text color
+label_Text_color: str = "{r}, {g}, {b}, {a}".format(r=157, g=154, b=171, a=255)  # label Text color
 
 tab_selected_bg_color = "{r}, {g}, {b}, {a}".format(r=60, g=120, b=160, a=255)  # tab selected background color
 tab_selected_text_color = "{r}, {g}, {b}, {a}".format(r=255, g=255, b=255, a=255)  # tab selected Text color
@@ -27,10 +27,13 @@ login_bg_color = "{r}, {g}, {b}, {a}".format(r=255, g=255, b=255, a=255)  # logi
 
 # trade_on_bg_color = "{r}, {g}, {b}, {a}".format(r=255, g=255, b=255, a=255)  # trade on background color
 trade_on_bg_color = "transparent"  # trade on background color
-trade_on_text_color = "{r}, {g}, {b}, {a}".format(r=0, g=255, b=0, a=255)  # trade on text color
+trade_on_text_color = "{r}, {g}, {b}, {a}".format(r=43, g=171, b=64, a=255)  # trade on text color
 # trade_off_bg_color = "{r}, {g}, {b}, {a}".format(r=255, g=255, b=255, a=255)  # trade off background color
+trade_none_bg_color = "transparent"  # trade off background color
+trade_none_text_color = "{r}, {g}, {b}, {a}".format(r=255, g=40, b=40, a=255)  # trade off text color
+
 trade_off_bg_color = "transparent"  # trade off background color
-trade_off_text_color = "{r}, {g}, {b}, {a}".format(r=255, g=40, b=40, a=255)  # trade off text color
+trade_off_text_color = "{r}, {g}, {b}, {a}".format(r=255, g=175, b=40, a=255)  # trade off text color
 
 DA_unit_ok_status_label_bot_bg = "{r}, {g}, {b}, {a}".format(r=19, g=164, b=70,
                                                              a=255)  # da units label back ground color
