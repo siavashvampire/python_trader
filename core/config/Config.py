127ec521befad7b2dfa0bd6db45fa0f051dbee15
siavash <sepahisiavash@gmail.com>
2023-07-18 15:06:08 +0330

    cleaning

--- core/config/Config.py
+++ core/config/Config.py
@@ -64,7 +64,7 @@ if __name__ in ['__main__', 'core.config.Config']:
         # Start Developer Config
         developer_config = sAll["developer_config"]
 
-        # login_developer = False
-        # if fernet.decrypt(developer_config) == b'VamPire1468':
-        #     login_developer = True
+        login_developer = False
+        if fernet.decrypt(developer_config) == b'VamPire1468':
+            login_developer = True
         # end  Developer Config
