476003c05673aefdc9163b8be30d1b3bcc2c072b
siavash <sepahisiavash@gmail.com>
2023-07-31 13:07:10 +0330

    make max_df_size

--- app/data_gathering/main.py
+++ app/data_gathering/main.py
@@ -1,7 +1,7 @@
-import os
 from datetime import datetime, timedelta
 import pandas as pd
 
+from core.app_provider.main import file_exist
 from core.config.Config import time_format
 from app.data_connector.model.data_connector import DataConnector
 
@@ -10,15 +10,7 @@ data_connector = DataConnector()
 candle = "M1"
 
 main_path = "File/trade_data/"
-
-
-def file_exist(file_name_in: str):
-    files = os.listdir(main_path)
-    for file in files:
-        if file_name_in == file:
-            return True
-
-    return False
+max_df_size = 60500
 
 
 def get_start_time_from_file(file_path_in: str) -> datetime:
@@ -27,7 +19,7 @@ def get_start_time_from_file(file_path_in: str) -> datetime:
         start_time_temp = datetime.strptime(data_temp.tail(1)['time'].values[0], time_format).replace(second=0,
                                                                                                       microsecond=0)
     except:
-        start_time_temp = datetime.utcnow() - timedelta(days=93)
+        start_time_temp = datetime.utcnow() - timedelta(days=2)
 
     return start_time_temp
 
@@ -40,7 +32,7 @@ def data_gathering(asset: str):
         force_otc = True
         file_path = main_path + "trade_data_history_" + asset + "_" + "otc_" + candle + ".csv"
 
-        if not file_exist("trade_data_history_" + asset + "_" + "otc_" + candle + ".csv"):
+        if not file_exist("trade_data_history_" + asset + "_" + "otc_" + candle + ".csv",main_path):
             start_time = datetime.utcnow() - timedelta(days=93)
             start_time = start_time.replace(second=0, microsecond=0)
         else:
@@ -48,12 +40,21 @@ def data_gathering(asset: str):
             start_time = start_time + timedelta(minutes=1)
 
         data_connector.get_history(asset, start_time.strftime(time_format), end_time.strftime(time_format),
-                                         candle, file_path, force_otc=force_otc)
+                                   candle, file_path, force_otc=force_otc)
+
+        try:
+            data = pd.read_csv(file_path)
+        except:
+            data = pd.DataFrame()
+
+
+        data = data.tail(max_df_size)
+        data.to_csv(file_path, index=False, encoding='utf-8')
 
         force_otc = False
         file_path = main_path + "trade_data_history_" + asset + "_" + candle + ".csv"
 
-        if not file_exist("trade_data_history_" + asset + "_" + candle + ".csv"):
+        if not file_exist("trade_data_history_" + asset + "_" + candle + ".csv",main_path):
             start_time = datetime.utcnow() - timedelta(days=93)
             start_time = start_time.replace(second=0, microsecond=0)
         else:
@@ -61,11 +62,10 @@ def data_gathering(asset: str):
             start_time = start_time + timedelta(minutes=1)
 
         data_connector.get_history(asset, start_time.strftime(time_format), end_time.strftime(time_format),
-                                         candle, file_path, force_otc=force_otc)
+                                   candle, file_path, force_otc=force_otc)
 
         try:
             data = pd.read_csv(file_path)
-            data = data.tail(10000)
         except:
             data = pd.DataFrame()
 
@@ -74,4 +74,5 @@ def data_gathering(asset: str):
             if row['o'] == temp and row['c'] == temp and row['h'] == temp and row['l'] == temp:
                 data = data.drop(index)
 
+        data = data.tail(max_df_size)
         data.to_csv(file_path, index=False, encoding='utf-8')
