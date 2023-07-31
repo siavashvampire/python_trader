c54be96f5b18339a7c71b48541619fdb9332df04
siavash <sepahisiavash@gmail.com>
2023-08-01 01:02:14 +0330

    add last_modified to update model

--- update_models.py
+++ update_models.py
@@ -4,6 +4,8 @@ from time import sleep
 from app.market_trading.model.trading_thread_model import TradingThreadModel
 from core.database.database import create_db
 
+import os.path
+
 create_db()
 
 from app.market_trading.api import get_all_trading, get_trading
@@ -14,12 +16,15 @@ trades = get_all_trading()
 for trade in trades:
     name = trade.currency_disp()
     print(name)
+    
     thread = TradingThreadModel(trade, None, None, None, None)
-    thread.ml_trading.counter = 730
-    start_train_time = datetime.now()
-    thread.model_thread.start()
-    sleep(5)
-    thread.stop_thread = True
-    thread.model_thread.join()
-    end_train_time = datetime.now()
-    print("total train time for ", name, " is : ", (end_train_time - start_train_time).total_seconds(), "seconds")
+    last_modified = datetime.fromtimestamp(os.path.getmtime(thread.ml_trading.model_name))
+    if (datetime.now() - last_modified).total_seconds() > 1 * 60 * 60:
+        thread.ml_trading.counter = 730
+        start_train_time = datetime.now()
+        thread.model_thread.start()
+        sleep(5)
+        thread.stop_thread = True
+        thread.model_thread.join()
+        end_train_time = datetime.now()
+        print("total train time for ", name, " is : ", (end_train_time - start_train_time).total_seconds(), "seconds")
