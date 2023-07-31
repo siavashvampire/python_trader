e06fc38b1d57a4b4d5b00e47a14b6445e896b284
siavash <sepahisiavash@gmail.com>
2023-07-31 13:09:01 +0330

    add update models

---
+++ update_models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from time import sleep
+
+from app.market_trading.model.trading_thread_model import TradingThreadModel
+from core.database.database import create_db
+
+create_db()
+
+from app.market_trading.api import get_all_trading
+
+trades = get_all_trading()
+
+for trade in trades:
+    name = trade.currency_disp()
+    print(name)
+    thread = TradingThreadModel(trade, None, None, None, None)
+    thread.ml_trading.counter = 730
+    start_train_time = datetime.now()
+    thread.model_thread.start()
+    sleep(5)
+    thread.stop_thread = False
+    thread.model_thread.join()
+    end_train_time = datetime.now()
+    print("total train time for ", name, " is : ", (end_train_time - start_train_time).total_seconds(), "seconds")
