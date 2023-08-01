b378e4ebd1138c9a50ac8e31f2e1df78a3f66706
siavash <sepahisiavash@gmail.com>
2023-08-01 09:48:47 +0330

    fix

--- update_models.py
+++ update_models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from time import sleep
 
 from app.market_trading.model.trading_thread_model import TradingThreadModel
+from core.app_provider.main import file_exist
 from core.database.database import create_db
 
 import os.path
@@ -16,15 +17,17 @@ trades = get_all_trading()
 for trade in trades:
     name = trade.currency_disp()
     print(name)
-    
+
     thread = TradingThreadModel(trade, None, None, None, None)
-    last_modified = datetime.fromtimestamp(os.path.getmtime(thread.ml_trading.model_name))
-    if (datetime.now() - last_modified).total_seconds() > 1 * 60 * 60:
-        thread.ml_trading.counter = 730
-        start_train_time = datetime.now()
-        thread.model_thread.start()
-        sleep(5)
-        thread.stop_thread = True
-        thread.model_thread.join()
-        end_train_time = datetime.now()
-        print("total train time for ", name, " is : ", (end_train_time - start_train_time).total_seconds(), "seconds")
+    if file_exist(thread.ml_trading.model_name, 'File/trade_models/'):
+        last_modified = datetime.fromtimestamp(os.path.getmtime(thread.ml_trading.model_name))
+        if (datetime.now() - last_modified).total_seconds() > 1 * 60 * 60:
+            thread.ml_trading.counter = 730
+            start_train_time = datetime.now()
+            thread.model_thread.start()
+            sleep(5)
+            thread.stop_thread = True
+            thread.model_thread.join()
+            end_train_time = datetime.now()
+            print("total train time for ", name, " is : ", (end_train_time - start_train_time).total_seconds(),
+                  "seconds")
